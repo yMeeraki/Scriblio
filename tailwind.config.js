0a0256caaa6e8bdf56e6c4589d5ecc13123a2f0d
yMeerki <yuktigupta95@gmail.com>
2025-04-15 15:49:11 +0530

    Adding Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2f4858",
+        secondary: "#00beff",
+        tertiary:"#00c9f8",
+        accent:"#ffb343"
+      },
+      
+    },
+  },
+  plugins: [],
+}
+
