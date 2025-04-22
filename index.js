68bd6f38047f7f1c955ceebf5ce5b57eb2abfe2c
yMeerki <yuktigupta95@gmail.com>
2025-04-22 15:17:07 +0530

    Create Blog

---
+++ index.js
@@ -0,0 +1,7 @@
+function openBlog() {
+    document.getElementById("create-blog-form").style.width = "100%";
+}
+
+function closeBlog() {
+    document.getElementById("create-blog-form").style.width = "0%";
+}
